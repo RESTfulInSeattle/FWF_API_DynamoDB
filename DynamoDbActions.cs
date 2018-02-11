6c2ae14131a4477e051d654434297f51b1350ba5
David Clayton <david@davidclayton.me>
2018-02-10 16:11:43 -0800

    Project Creation

---
+++ DynamoDbActions.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace FrequentWordFinderAPI.Controllers
+{
+    public class DynamoDbActions
+    {
+        public static string RetrieveItem(string url)
+        {
+            try
+            {
+                var client = new AmazonDynamoDBClient();
+                var context = new DynamoDBContext(client);
+                var item = context.Load<DynamoDBItem>(url, "result");
+
+                return item.result;
+            }
+            catch (AmazonDynamoDBException e)
+            {
+                return "";
+            }
+        }
+
+        public static bool AddItem(string url, string result)
+        {
+            try
+            {
+                var client = new AmazonDynamoDBClient();
+                var context = new DynamoDBContext(client);
+                var item = new DynamoDBItem
+                {
+                    Id = url,
+                    result = result
+                };
+
+                context.Save(item);
+            }
+            catch (AmazonDynamoDBException e)
+            {
+                return false;
+            }
+
+            return true;
+
+
+        }
+    }
+}
